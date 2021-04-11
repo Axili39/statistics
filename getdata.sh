a0e254e77f835462dd91d76bfe9d6969a8bd9778
Axili39 <axili@me.com>
2021-04-11 12:33:21 +0000

    added checkall script

--- getdata.sh
+++ getdata.sh
@@ -1,1 +1,2 @@
-curl "https://query1.finance.yahoo.com/v7/finance/download/AI.PA?period1=946857600&period2=1618012800&interval=1d&events=history&includeAdjustedClose=true" -o AI.PA.csv
+TICKER=ORA.PA
+curl "https://query1.finance.yahoo.com/v7/finance/download/${TICKER}?period1=946857600&period2=1618012800&interval=1d&events=history&includeAdjustedClose=true" -o ${TICKER}.csv
--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"os"
-	"log"
 	"encoding/csv"
-	"strconv"
-	"io"
 	"fmt"
 	"github.com/montanaflynn/stats"
+	"io"
+	"log"
+	"os"
+	"strconv"
 	"time"
+	"flag"
 )
 
-//StockRecord: Stock Record element 
+//StockRecord: Stock Record element
 type StockRecord struct {
 	Date time.Time
 	Open float64
 }
 
-func load() []StockRecord {
-		// Open the file
-	csvfile, err := os.Open("AI.PA.csv")
+func load(ticker string, after time.Time) []StockRecord {
+	// Open the file
+	csvfile, err := os.Open(ticker + ".csv")
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
 	}
@@ -30,7 +31,7 @@ func load() []StockRecord {
 	// Read labels
 	_, err = r.Read()
 	if err == io.EOF {
-			return nil
+		return nil
 	}
 	var records []StockRecord
 
@@ -45,57 +46,62 @@ func load() []StockRecord {
 			log.Fatal(err)
 		}
 		/*
-		for index := range labels {
-			fmt.Printf(" %s: %s", labels[index], record[index]);
-		}
-		fmt.Println()
+			for index := range labels {
+				fmt.Printf(" %s: %s", labels[index], record[index]);
+			}
+			fmt.Println()
 		*/
 		layout := "2006-01-02"
 		t, err := time.Parse(layout, record[0])
 		if err != nil {
-			log.Fatal(err)
+			continue
 		}
-      // Parse the grade value.
-      open, err := strconv.ParseFloat(record[1], 64)
-      if err != nil {
-          log.Fatal(err)
-      }
-		records = append(records, StockRecord{t, open})
-		
+		// Parse the grade value.
+		open, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			continue
+		}
+		if t.Sub(after).Seconds() > 0 {
+			records = append(records, StockRecord{t, open})
+
+		}
+
 	}
 	//fmt.Println(records)
 	return records
 }
-func regression(records []StockRecord) {
+func compute(ticker string, records []StockRecord) {
 	// Convert raw data to serie
 	var serie stats.Series
 	for index := range records {
-		serie = append(serie, stats.Coordinate{records[index].Date.Sub(records[0].Date).Seconds(), records[index].Open})		
+		serie = append(serie, stats.Coordinate{records[index].Date.Sub(records[0].Date).Seconds(), records[index].Open})
 	}
 
-	reg, err := stats.LinearRegression(serie) 
+	reg, err := stats.LinearRegression(serie)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println(reg)
+	var sample stats.Float64Data
+	for index := range reg {
+		sample = append(sample, serie[index].Y-reg[index].Y)
+	}
+	stddev, _ := stats.StandardDeviation(sample)
+
+	/*
+	for index := range reg {
+		deltaStd := (serie[index].Y-reg[index].Y)/stddev
+		fmt.Println("date :", records[index].Date, " open:", serie[index].Y, " reg:", reg[index].Y, " dY:", serie[index].Y-reg[index].Y, " dStd:", deltaStd)
+	}
+	*/
+	index := len(records)-1
+	deltaStd := (serie[index].Y-reg[index].Y)/stddev
+	fmt.Println(ticker, "date :", records[index].Date, " open:", serie[index].Y, " reg:", reg[index].Y, " dY:", serie[index].Y-reg[index].Y, " dStd:", deltaStd, "standard dev:", stddev)
 }
-func calc() {
-	// start with some source data to use
-	data := []float64{1.0, 2.1, 3.2, 4.823, 4.1, 5.8}
 
-	// you could also use different types like this
-	// data := stats.LoadRawData([]int{1, 2, 3, 4, 5})
-	// data := stats.LoadRawData([]interface{}{1.1, "2", 3})
-	// etc...
-
-	median, _ := stats.Median(data)
-	fmt.Println(median) // 3.65
-
-	roundedMedian, _ := stats.Round(median, 0)
-	fmt.Println(roundedMedian) // 4
-}
 func main() {
-	fmt.Println("hello")
-	data := load()
-	regression(data)
+	var ticker = flag.String("t", "ORA.PA", "ticker to check")
+	flag.Parse()
+	data := load(*ticker, time.Now().AddDate(-10, 0, 0))
+	compute(*ticker, data)
 }
