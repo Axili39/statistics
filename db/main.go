6033ac8a94cd342d1eafaa03d95d781ce56088d5
Axili39 <axili@me.com>
2021-04-12 04:29:06 +0000

    added stock candidate detection

--- db/main.go
+++ db/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"net/http"
 	"database/sql"
 	"encoding/csv"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 	"io"
 	"log"
+	"net/http"
+	"time"
+	"github.com/montanaflynn/stats"
+	_ "github.com/mattn/go-sqlite3"
 	//"os"
 )
 func anime(iterator int) {
@@ -16,8 +18,8 @@ func anime(iterator int) {
 }
 func load(db *sql.DB, ticker string) {
 	// direct download from yahoo
-	url_base := "https://query1.finance.yahoo.com/v7/finance/download/"
-	url := url_base + ticker + "?period1=946857600&period2=1618012800&interval=1d&events=history&includeAdjustedClose=true"
+	urlBase := "https://query1.finance.yahoo.com/v7/finance/download/"
+	url := urlBase + ticker + "?period1=946857600&period2=1618012800&interval=1d&events=history&includeAdjustedClose=true"
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(ticker, ": ", err)
@@ -78,12 +80,8 @@ func load(db *sql.DB, ticker string) {
 	fmt.Println("\tlast id: ", id, last)
 }
 
-func main() {
-	db, err := sql.Open("sqlite3", "./db.sql")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+func initdb(db *sql.DB) {
+
 	//
 	rows, err := db.Query("SELECT ticker, name  FROM tickers")
     if err != nil {
@@ -113,4 +111,86 @@ func main() {
 		fmt.Println("Loading ticker :", r.ticker, " ", r.name)
 		load(db, r.ticker)
 	}
+}
+
+// Compute linear regression serie and find candidate
+func compute(db *sql.DB, ticker string, after time.Time, criteria float64) {
+	// load
+	rows, err := db.Query("SELECT date, close FROM eod WHERE close <> \"null\" and ticker = \"" + ticker + "\" and date > \"" + after.Format("2006-01-02") + "\"")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+	var serie stats.Series
+
+	var date string
+	var close float64
+	for rows.Next() {
+		err = rows.Scan(&date, &close)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		t, err := time.Parse("2006-01-02T00:00:00Z", date)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		serie = append(serie, stats.Coordinate{t.Sub(after).Seconds(), close})
+	}
+
+	reg, err := stats.LinearRegression(serie)
+	if err != nil {
+		fmt.Println("regression error:",err)
+		return
+	}
+	var sample stats.Float64Data
+	for index := range reg {
+		sample = append(sample, serie[index].Y-reg[index].Y)
+	}
+	stddev, _ := stats.StandardDeviation(sample)
+
+	/*
+	for index := range reg {
+		deltaStd := (serie[index].Y-reg[index].Y)/stddev
+		fmt.Println("date :", records[index].Date, " open:", serie[index].Y, " reg:", reg[index].Y, " dY:", serie[index].Y-reg[index].Y, " dStd:", deltaStd)
+	}
+	*/
+	index := len(serie)-1
+	deltaStd := (serie[index].Y-reg[index].Y)/stddev
+	if deltaStd < criteria {
+		fmt.Println(ticker, "date :", after.Add(time.Duration((serie[index].X))*time.Second), " open:", serie[index].Y, " reg:", reg[index].Y, " dY:", serie[index].Y-reg[index].Y, " dStd:", deltaStd, "standard dev:", stddev)
+	}
+}
+
+func findCandidate(db *sql.DB) {
+	rows, err := db.Query("SELECT ticker, name  FROM tickers")
+    if err != nil {
+		log.Fatal(err)
+	}
+	var ticker string
+	var name string
+	for rows.Next() {
+		err = rows.Scan(&ticker, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("computing ", ticker)
+		compute(db, ticker, time.Now().AddDate(-10,0,0), 20)
+		
+	}
+	defer rows.Close()
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	//initdb(db)
+	findCandidate(db)
 }
