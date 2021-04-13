95fa1d6a96aec1152d30be31644d43070631f753
Axili39 <axili@me.com>
2021-04-13 21:37:53 +0000

    added marketstack provider, not finished yet

---
+++ pkg/provider/marketstack/marketstack.go
@@ -0,0 +1,37 @@
+package marketstack
+
+import (
+	"github.com/Axili39/statistics/pkg/provider"
+	"time"
+	"net/http"
+	"fmt"
+	"encoding/csv"
+	"io"
+)
+
+const urlBase string = "http://api.marketstack.com/v1/eod"
+
+type MarketstackProvider struct {
+	ApiKey string
+}
+
+func (p *MarketstackProvider) RetrieveData(ticker string, from time.Time, to time.Time) ([]provider.EodRecord, error) {
+	// TODO if limit exceeds Limit, makes multiple query
+	url := urlBase +
+		"?access_key=" + p.ApiKey +
+		"&symbols=" + ticker +
+		"&sort=DESC" +
+		"&date_from=" + from.Format("2010-01-02") +
+		"&date_to=" + to.Format("2010-01-02") +
+		"&limit=50000" +
+		"&offset=0"
+	
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(ticker, ": ", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// TODO unmarshall to EodRecord
+}
